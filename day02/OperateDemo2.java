35f63cc4f423e0312a5fd9a6b9f1deb7b8816e17
zxpsuper <zxpsuper@163.com>
2018-04-12 19:12:35 +0800

    0

---
+++ day02/OperateDemo2.java
@@ -0,0 +1,18 @@
+class OperateDemo2 
+{
+	public static void main(String[] args) 
+	{
+		//¸³ÖµÔËËã·û¡£=  += -= *= /= %=
+		//int a,b,c;
+		//a = b = c = 4;
+
+		//int a = 4;
+		//a+=2;//a = a + 2;
+
+		short s = 3;
+		//s+=4;
+	
+		s = (byte)(s + 18888);
+		System.out.println("s="+s);
+	}
+}
---
+++ day03/OperateDemo4.java
@@ -0,0 +1,20 @@
+class OperateDemo4 
+{
+	public static void main(String[] args) 
+	{
+		// 且运算
+		// 0110
+		// 0011
+		// 0010 === 2
+		System.out.println(6&3);
+		// 或运算
+		// 0110
+		// 0011
+		// 0111 === 7
+		System.out.println(6|3);
+		// 加一再取反 -8
+		System.out.println(~7); 
+		System.out.println(3<<2);//3左移两位。
+
+	}
+}
